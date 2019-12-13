b092416e3804a3c2dd2b2b8f7200220edb508d69
ronakkumarkb3684 <36426320+ronakkumarkb3684@users.noreply.github.com>
2019-12-13 00:35:24 -0500

    Add files via upload

---
+++ views/abstract-views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>IS218 Project</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<!-- the body section -->
+<body>
+
+
+<!--<div class="container">-->
+<!--    <header><h1>IS218 Project</h1></header>-->
