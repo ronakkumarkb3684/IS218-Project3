86c883ad28f83bd484a62d8d779d65df153ee5a6
ronakkumarkb3684 <36426320+ronakkumarkb3684@users.noreply.github.com>
2019-12-20 14:07:50 -0500

    Add files via upload

--- views/display_all_questions.php
+++ views/display_all_questions.php
@@ -1,8 +1,9 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>All User's Question</title>
     <link rel="stylesheet" type="text/css" href="../css/main.css">
 
 </head>
@@ -12,30 +13,30 @@
 
 
     <h1> Questions of  <?php echo get_username($userId);
-        $questions = get_users_questions($userId);
+        /*$questions = get_users_questions($userId);*/
         ?></h1>
-    <a href=".?action=display_question_form&userId=<?php echo $userId ?>"><input type="submit" value="Add New Question"></a><br>
 
-   <!-- <form action="index.php" method="post">
-        <input type="hidden" name="action" value="view_others_questions">
-        <input type="hidden" name="userId" value="<?php /*echo $userId; */?>">
-        <input type="submit" value="View Others Questions" class="btn">
-    </form>-->
+    <form action="index.php" method="get">
+        <input type="hidden" name="action" value="display_questions">
+        <input type="hidden" name="userId" value="<?php echo $userId; ?>">
+        <input type="submit" value="Back to Your Questions" class="btn">
+    </form>
 
-    <a href=".?action=view_others_questions&userId=<?php echo $userId; ?>">View Other Questions</a>
-
+    <a href=".?action=display_question_form&userId=><?php echo $userId ?>"><input type="submit" value="Add New Question"></a>
     <table align="center">
         <tr>
             <th>Id</th>
+            <th>User</th>
             <th>Name</th>
             <th>Body</th>
             <th>Skills</th>
             <th>View</th>
-            <th>Delete</th>
+            <!--            <th>Delete</th>-->
         </tr>
         <?php foreach($questions as $question) : ?>
             <tr>
                 <td><?php echo $question['id'];?></td>
+                <td><?php echo $user = get_username($question['ownerid']); ?></td>
                 <td><?php echo $question['title']; ?></td>
                 <td><?php echo $question['body']; ?></td>
                 <td><?php echo $question['skills']; ?></td>
@@ -48,15 +49,15 @@
                         <button><input type="submit" class="btn edit" value="View"></button>
                     </form>
                 </td>
-                <td>
+                <!--<td>
                     <form action="index.php" method="post">
                         <input type="hidden" name="action" value="delete_question">
-                        <input type="hidden" name="questionId" value="<?php echo $question['id'];?>">
-                        <input type="hidden" name="userId" value="<?php echo $userId ?>">
+                        <input type="hidden" name="questionId" value="<?php /*echo $question['id'];*/?>">
+                        <input type="hidden" name="userId" value="<?php /*echo $userId */?>">
 
                         <button><input class="btn" type="submit" value="Delete"></button>
                     </form>
-                </td>
+                </td>-->
             </tr>
         <?php endforeach; ?>
     </table>
@@ -65,3 +66,4 @@
 </main>
 </body>
 </html>
+
--- views/display_single_question.php
+++ views/display_single_question.php
@@ -1,8 +1,9 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>Single Question</title>
     <link rel="stylesheet" type="text/css" href="../css/main.css">
 
 </head>
@@ -12,26 +13,17 @@
 
 
     <h1> Questions of  <?php echo get_username($userId);
-        $questions = get_users_questions($userId);
+        /*$questions = get_users_questions($userId);*/
         ?></h1>
-    <a href=".?action=display_question_form&userId=<?php echo $userId ?>"><input type="submit" value="Add New Question"></a><br>
-
-   <!-- <form action="index.php" method="post">
-        <input type="hidden" name="action" value="view_others_questions">
-        <input type="hidden" name="userId" value="<?php /*echo $userId; */?>">
-        <input type="submit" value="View Others Questions" class="btn">
-    </form>-->
-
-    <a href=".?action=view_others_questions&userId=<?php echo $userId; ?>">View Other Questions</a>
-
+<!--    <a href=".?action=display_question_form&userId=--><?php //echo $userId ?><!--"><input type="submit" value="Add New Question"></a>-->
     <table align="center">
         <tr>
             <th>Id</th>
             <th>Name</th>
             <th>Body</th>
             <th>Skills</th>
-            <th>View</th>
-            <th>Delete</th>
+            <th>Go Back</th>
+<!--            <th>Delete</th>-->
         </tr>
         <?php foreach($questions as $question) : ?>
             <tr>
@@ -41,22 +33,21 @@
                 <td><?php echo $question['skills']; ?></td>
                 <td>
                     <form>
-                        <input type="hidden" name="action" value="single_question">
+                        <input type="hidden" name="action" value="display_questions">
                         <input type="hidden" name="questionId" value="<?php echo $question['id']; ?>">
                         <input type="hidden" name="userId" value="<?php echo $userId ?>">
-
-                        <button><input type="submit" class="btn edit" value="View"></button>
+                        <button><input type="submit" class="btn edit" value="Go Back" ></button>
                     </form>
                 </td>
-                <td>
+                <!--<td>
                     <form action="index.php" method="post">
                         <input type="hidden" name="action" value="delete_question">
-                        <input type="hidden" name="questionId" value="<?php echo $question['id'];?>">
-                        <input type="hidden" name="userId" value="<?php echo $userId ?>">
+                        <input type="hidden" name="questionId" value="<?php /*echo $question['id'];*/?>">
+                        <input type="hidden" name="userId" value="<?php /*echo $userId */?>">
 
                         <button><input class="btn" type="submit" value="Delete"></button>
                     </form>
-                </td>
+                </td>-->
             </tr>
         <?php endforeach; ?>
     </table>
